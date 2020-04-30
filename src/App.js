058bb78a5c0ae46c80ba0698eb44794e37c625a6
shashank2704 <47267540+shashank2704@users.noreply.github.com>
2020-04-30 18:13:14 +0530

    My 1St Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from "./Header.js";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+   
+        <Header/>
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">ADD</button>
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+
+        </div>
+      </div>
+
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header=function()
+{
+   return(
+        
+        <div className="header">
+        Phone Directory
+      </div>
+    )
+}
+
+
+
+export default Header;
